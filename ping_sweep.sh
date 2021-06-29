d1039741952df2f4e71b9a19f81fac3a8bc0dce9
nmmorette <85892001+nmmorette@users.noreply.github.com>
2021-06-28 23:27:59 -0300

    Update ping_sweep.sh

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+###ping_sweep.sh
+###---------------------------------------------------------------
+### Objective: Ping all hosts in the network
+###---------------------------------------------------------------
+### Author: Natan Morette
+###---------------------------------------------------------------
+
+
+
 if [ "$1" == "" ]
 then
-	echo "DESEC SECURITY - PING SWEEP"
-	echo "Modeo de uso : $0 REDE"
-	echo "Exemplo: $0 192.168.0."
+	echo "PING SWEEP"
+	echo "Use mode : $0 REDE"
+	echo "Example: $0 192.168.0."
 else
 	for host in {1..254};
 	do
