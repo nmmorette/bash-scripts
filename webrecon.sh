22f384d3ed78699c8022be9e947457bdcc10ab87
nmmorette <85892001+nmmorette@users.noreply.github.com>
2021-06-29 00:13:11 -0300

    Update webrecon.sh

--- webrecon.sh
+++ webrecon.sh
@@ -9,7 +9,7 @@
 ###---------------------------------------------------------------
 for palavra in $(cat "INPUT YOUR WORDLIST HERE, download on here:https://github.com/nmmorette/worldlist)
 do
-resp=$(curl -s -H "User-Agent: SimasTurbando " -o /dev/null -w "%{http_code}" $1/$palavra/)
+resp=$(curl -s -H "User-Agent: Tony-Stark" -o /dev/null -w "%{http_code}" $1/$palavra/)
 if  [[ $resp == "200" ]]
 then
 echo " Diretorio encontrado: http://$1/$palavra"
